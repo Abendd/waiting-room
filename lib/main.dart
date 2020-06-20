f22f3201db58a99be77fa1ef679a4552cab35717
Kartik Tyagi <ktkartiktyagi98@gmail.com>
2020-06-20 16:24:07 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,380 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:jitsi_meet/jitsi_meet.dart';
+import 'package:jitsi_meet/jitsi_meeting_listener.dart';
+import 'package:jitsi_meet/room_name_constraint.dart';
+import 'package:jitsi_meet/room_name_constraint_type.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title',
+      home: WaitingRoom(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('----------1');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('HomePage')),
+    );
+  }
+}
+
+class WaitingRoom extends StatefulWidget {
+  @override
+  _WaitingRoomState createState() => _WaitingRoomState();
+}
+
+class _WaitingRoomState extends State<WaitingRoom> {
+  String studentId = '17103348';
+  String currentClass = '10';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getdata();
+  }
+
+  getdata() async {
+    var data = await Firestore.instance
+        .collection('lobby')
+        .document(currentClass)
+        .get();
+    print('------');
+    List<String> details = [];
+    bool flag = false;
+    for (int i = 0; i < data['waiting room'].length; i++) {
+      if (data['waiting room'].elementAt(i) == studentId) {
+        flag = true;
+        break;
+      }
+      details.add(data['waiting room'].elementAt(i));
+    }
+    if (!flag) {
+      details.add(studentId);
+      Firestore.instance
+          .collection('lobby')
+          .document(currentClass)
+          .setData({'waiting room': details});
+    }
+    print(data['waiting room']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder(
+            stream: Firestore.instance
+                .collection('lobby')
+                .document(currentClass)
+                .snapshots(),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.hasData) {
+                int position = -1;
+                print(snapshot.data['waiting room']);
+                for (int i = 0; i < snapshot.data['waiting room'].length; i++) {
+                  if (snapshot.data['waiting room'].elementAt(i) == studentId) {
+                    position = i + 1;
+                    break;
+                  }
+                }
+                if (position == 1) {
+                   Navigator.push(context, MaterialPageRoute(builder:(context)=> Meeting() ));
+                }
+                return Center(child: Text(position.toString()));
+              }else{
+                return Text('loading');
+              }
+            }));
+  }
+}
+
+class Meeting extends StatefulWidget {
+  @override
+  _MeetingState createState() => _MeetingState();
+}
+
+class _MeetingState extends State<Meeting> {
+  final serverText = TextEditingController();
+  final roomText = TextEditingController(text: "plugintestroom");
+  final subjectText = TextEditingController(text: "My Plugin Test Meeting");
+  final nameText = TextEditingController(text: "Plugin Test User");
+  final emailText = TextEditingController(text: "fake@email.com");
+  final iosAppBarRGBAColor =
+      TextEditingController(text: "#0080FF80"); //transparent blue
+  var isAudioOnly = true;
+  var isAudioMuted = true;
+  var isVideoMuted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    JitsiMeet.addListener(JitsiMeetingListener(
+        onConferenceWillJoin: _onConferenceWillJoin,
+        onConferenceJoined: _onConferenceJoined,
+        onConferenceTerminated: _onConferenceTerminated,
+        onError: _onError));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    JitsiMeet.removeAllListeners();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 24.0,
+                ),
+                TextField(
+                  controller: serverText,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Server URL",
+                      hintText: "Hint: Leave empty for meet.jitsi.si"),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: roomText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Room",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: subjectText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Subject",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: nameText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Display Name",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: emailText,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CheckboxListTile(
+                  title: Text("Audio Only"),
+                  value: isAudioOnly,
+                  onChanged: _onAudioOnlyChanged,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CheckboxListTile(
+                  title: Text("Audio Muted"),
+                  value: isAudioMuted,
+                  onChanged: _onAudioMutedChanged,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CheckboxListTile(
+                  title: Text("Video Muted"),
+                  value: isVideoMuted,
+                  onChanged: _onVideoMutedChanged,
+                ),
+                Divider(
+                  height: 48.0,
+                  thickness: 2.0,
+                ),
+                SizedBox(
+                  height: 64.0,
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _joinMeeting();
+                    },
+                    child: Text(
+                      "Join Meeting",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                  ),
+                ),
+                Divider(
+                  height: 10.0,
+                ),
+                SizedBox(
+                  height: 64.0,
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    onPressed: () async {
+                      var data = await Firestore.instance
+                          .collection('lobby')
+                          .document('10')
+                          .get();
+                      print('------');
+                      List<String> details = [];
+                      for (int i = 1; i < data['waiting room'].length; i++) {
+                        details.add(data['waiting room'].elementAt(i));
+                      }
+                      Firestore.instance
+                          .collection('lobby')
+                          .document('10')
+                          .setData({'waiting room': details});
+                      print('-----------11');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+
+                      print('-----------1111');
+                    },
+                    child: Text(
+                      "Leave Meeting",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _onAudioOnlyChanged(bool value) {
+    setState(() {
+      isAudioOnly = value;
+    });
+  }
+
+  _onAudioMutedChanged(bool value) {
+    setState(() {
+      isAudioMuted = value;
+    });
+  }
+
+  _onVideoMutedChanged(bool value) {
+    setState(() {
+      isVideoMuted = value;
+    });
+  }
+
+  _joinMeeting() async {
+    String serverUrl =
+        serverText.text?.trim()?.isEmpty ?? "" ? null : serverText.text;
+
+    try {
+      var options = JitsiMeetingOptions()
+        ..room = roomText.text
+        ..serverURL = serverUrl
+        ..subject = subjectText.text
+        ..userDisplayName = nameText.text
+        ..userEmail = emailText.text
+        ..iosAppBarRGBAColor = iosAppBarRGBAColor.text
+        ..audioOnly = isAudioOnly
+        ..audioMuted = isAudioMuted
+        ..videoMuted = isVideoMuted;
+
+      debugPrint("JitsiMeetingOptions: $options");
+      await JitsiMeet.joinMeeting(
+        options,
+        listener: JitsiMeetingListener(onConferenceWillJoin: ({message}) {
+          debugPrint("${options.room} will join with message: $message");
+        }, onConferenceJoined: ({message}) {
+          debugPrint("${options.room} joined with message: $message");
+        }, onConferenceTerminated: ({message}) {
+          debugPrint("${options.room} terminated with message: $message");
+        }),
+        // by default, plugin default constraints are used
+        //roomNameConstraints: new Map(), // to disable all constraints
+        //roomNameConstraints: customContraints, // to use your own constraint(s)
+      );
+    } catch (error) {
+      debugPrint("error: $error");
+    }
+  }
+
+  static final Map<RoomNameConstraintType, RoomNameConstraint>
+      customContraints = {
+    RoomNameConstraintType.MAX_LENGTH: new RoomNameConstraint((value) {
+      return value.trim().length <= 50;
+    }, "Maximum room name length should be 30."),
+    RoomNameConstraintType.FORBIDDEN_CHARS: new RoomNameConstraint((value) {
+      return RegExp(r"[$€£]+", caseSensitive: false, multiLine: false)
+              .hasMatch(value) ==
+          false;
+    }, "Currencies characters aren't allowed in room names."),
+  };
+
+  void _onConferenceWillJoin({message}) {
+    debugPrint("_onConferenceWillJoin broadcasted with message: $message");
+  }
+
+  void _onConferenceJoined({message}) {
+    debugPrint("_onConferenceJoined broadcasted with message: $message");
+  }
+
+  void _onConferenceTerminated({message}) {
+    debugPrint("_onConferenceTerminated broadcasted with message: $message");
+  }
+
+  _onError(error) {
+    debugPrint("_onError broadcasted: $error");
+  }
+}
